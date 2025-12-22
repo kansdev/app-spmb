287a251534ab1fb4a55cba687d73f20341c7fefa
kansdev <mmaulana69@guru.smk.belajar.id>
2025-12-22 13:41:26 +0700

    Fixed bugs admin dashboard 22/12/2025

--- resources/views/admin/pendaftar.blade.php
+++ resources/views/admin/pendaftar.blade.php
@@ -17,38 +17,40 @@
         <h5>Data Calon Pendaftar</h5>
     </div>
     <div class="card-body">
-        <table class="table table-border table-striped" id="pendaftar">
-            <thead>
-                <tr>
-                    <th>No Pendaftaran</th>
-                    <th>Nama</th>
-                    <th>NIK</th>
-                    <th>Nilai Raport</th>
-                    <th>Jurusan 1</th>
-                    <th>Jurusan 2</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
-            <tbody class="table-border-bottom-0">
-                @foreach ($calon_pendaftar as $cp)
+        <div class="table-responsive">
+            <table class="table table-border table-striped w-100" id="pendaftar">
+                <thead>
                     <tr>
-                        <td>{{ $cp->nomor_pendaftaran}}</td>
-                        <td>{{ $cp->nama_siswa }}</td>
-                        <td>{{ $cp->nik }}</td>
-                        <td>{{ optional($cp->user->nilai_raport)->rata_rata}}</td>
-                        <td>{{ $cp->jurusan_pertama }}</td>
-                        <td>{{ $cp->jurusan_kedua }}</td>
-                        @if ($cp->status == 'Belum Terverifikasi')
-                            <td><a type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#verifikasi_{{ $cp->id }}">{{ $cp->status }}</button></td>
-                        @elseif($cp->status == 'Terverifikasi')
-                            <td><span class="badge bg-label-success">{{ $cp->status }}</span></td>
-                        @else
-                            <td><span class="badge bg-label-danger">{{ $cp->status }}</span></td>
-                        @endif
+                        <th>No Pendaftaran</th>
+                        <th>Nama</th>
+                        <th>NIK</th>
+                        <th>Nilai Raport</th>
+                        <th>Jurusan 1</th>
+                        <th>Jurusan 2</th>
+                        <th>Status</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody class="table-border-bottom-0">
+                    @foreach ($calon_pendaftar as $cp)
+                        <tr>
+                            <td>{{ $cp->nomor_pendaftaran}}</td>
+                            <td>{{ $cp->nama_siswa }}</td>
+                            <td>{{ $cp->nik }}</td>
+                            <td>{{ optional($cp->user->nilai_raport)->rata_rata}}</td>
+                            <td>{{ $cp->jurusan_pertama }}</td>
+                            <td>{{ $cp->jurusan_kedua }}</td>
+                            @if ($cp->status == 'Belum Terverifikasi')
+                                <td><a type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#verifikasi_{{ $cp->id }}">{{ $cp->status }}</button></td>
+                            @elseif($cp->status == 'Terverifikasi')
+                                <td><span class="badge bg-label-success">{{ $cp->status }}</span></td>
+                            @else
+                                <td><span class="badge bg-label-danger">{{ $cp->status }}</span></td>
+                            @endif
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 
     @foreach ($calon_pendaftar as $cp)
