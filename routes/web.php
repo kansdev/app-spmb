2653db171b8a29021d0a6381db0be5a06652998d
m-ade-maulana <ademaulana464@gmail.com>
2026-06-30 11:32:14 +0700

    30/06/2026

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,14 @@ use Carbon\Carbon;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\users\CekPendaftarController;
 use App\Http\Controllers\users\DashboardController;
-// use App\Http\Controllers\users\FormulirOrangTuaController;
-// use App\Http\Controllers\users\FormulirSiswaController;
-// use App\Http\Controllers\users\FormulirPeriodik;
-// use App\Http\Controllers\users\FormulirRegistrasi;
-// use App\Http\Controllers\users\FormulirNilaiRaport;
-// use App\Http\Controllers\test\UjianController;
+
+// Matikan sementara
+use App\Http\Controllers\users\FormulirOrangTuaController;
+use App\Http\Controllers\users\FormulirSiswaController;
+use App\Http\Controllers\users\FormulirPeriodik;
+use App\Http\Controllers\users\FormulirRegistrasi;
+use App\Http\Controllers\users\FormulirNilaiRaport;
+use App\Http\Controllers\test\UjianController;
 
 use Illuminate\Support\Facades\Storage;
 use Barryvdh\DomPDF\Facade\Pdf;
@@ -88,68 +90,68 @@ Route::middleware(['auth', 'preventBackHistory'])->group(function () {
 });
 
 // Middleware untuk formulir siswa
-// Route::middleware(['auth', 'preventBackHistory', 'checkProfile'])->group(function () {
-//     // Formulir orang tua
-//     Route::get('/formulir_siswa', [FormulirSiswaController::class, 'index'])->name('formulir_siswa');
-//     Route::post('/formulir_siswa/save', [FormulirSiswaController::class, 'save_siswa'])->name('save_siswa');
-//     Route::put('/formulir_siswa/edit/{siswa}', [FormulirSiswaController::class, 'edit_siswa'])->name('edit_siswa');
-// });
+Route::middleware(['auth', 'preventBackHistory', 'checkProfile'])->group(function () {
+    // Formulir orang tua
+    Route::get('/formulir_siswa', [FormulirSiswaController::class, 'index'])->name('formulir_siswa');
+    Route::post('/formulir_siswa/save', [FormulirSiswaController::class, 'save_siswa'])->name('save_siswa');
+    Route::put('/formulir_siswa/edit/{siswa}', [FormulirSiswaController::class, 'edit_siswa'])->name('edit_siswa');
+});
 
 // // Middleware untuk formulir orang tua
-// Route::middleware(['auth', 'preventBackHistory', 'checkProfile', 'checkDataSiswa'])->group(function () {
-//     // Formulir orang tua
-//     Route::get('/formulir_orang_tua', [FormulirOrangTuaController::class, 'index'])->name('formulir_orang_tua');
-//     Route::post('/formulir_orang_tua/save', [FormulirOrangTuaController::class, 'save_orang_tua'])->name('save_orang_tua');
-//     Route::put('/formulir_orang_tua/edit/{orang_tua}', [FormulirOrangTuaController::class, 'edit_orang_tua'])->name('edit_orang_tua');
-// });
+Route::middleware(['auth', 'preventBackHistory', 'checkProfile', 'checkDataSiswa'])->group(function () {
+    // Formulir orang tua
+    Route::get('/formulir_orang_tua', [FormulirOrangTuaController::class, 'index'])->name('formulir_orang_tua');
+    Route::post('/formulir_orang_tua/save', [FormulirOrangTuaController::class, 'save_orang_tua'])->name('save_orang_tua');
+    Route::put('/formulir_orang_tua/edit/{orang_tua}', [FormulirOrangTuaController::class, 'edit_orang_tua'])->name('edit_orang_tua');
+});
 
 // // Midleware untuk formulir periodik
-// Route::middleware(['auth', 'preventBackHistory', 'checkProfile', 'checkDataSiswa', 'checkDataOrangTua'])->group(function () {
-//     // Formulir Periodik
-//     Route::get('/formulir_periodik', [FormulirPeriodik::class, 'index'])->name('formulir_periodik');
-//     Route::post('/formulir_periodik/save', [FormulirPeriodik::class, 'save_periodik'])->name('save_periodik');
-//     Route::put('/formulir_periodik/edit/{periodik}', [FormulirPeriodik::class, 'edit_periodik'])->name('edit_periodik');
-// });
+Route::middleware(['auth', 'preventBackHistory', 'checkProfile', 'checkDataSiswa', 'checkDataOrangTua'])->group(function () {
+    // Formulir Periodik
+    Route::get('/formulir_periodik', [FormulirPeriodik::class, 'index'])->name('formulir_periodik');
+    Route::post('/formulir_periodik/save', [FormulirPeriodik::class, 'save_periodik'])->name('save_periodik');
+    Route::put('/formulir_periodik/edit/{periodik}', [FormulirPeriodik::class, 'edit_periodik'])->name('edit_periodik');
+});
 
 // // Middleware formulir untuk upload berkas dan id_card
-// Route::middleware(['auth', 'preventBackHistory', 'checkProfile', 'checkDataSiswa', 'checkDataOrangTua', 'checkDataPeriodik'])->group(function () {
-//     // route nilai raport
-//     Route::get('/formulir_nilai_raport', [FormulirNilaiRaport::class, 'index'])->name('formulir_nilai_raport');
-//     Route::post('/formulir_nilai_raport/save', [FormulirNilaiRaport::class, 'save_nilai_raport'])->name('save_nilai_raport');
-//     Route::put('/formulir_nilai_raport/edit/{nilai_raport}', [FormulirNilaiRaport::class, 'edit_nilai_raport'])->name('edit_nilai_raport');
+Route::middleware(['auth', 'preventBackHistory', 'checkProfile', 'checkDataSiswa', 'checkDataOrangTua', 'checkDataPeriodik'])->group(function () {
+    // route nilai raport
+    Route::get('/formulir_nilai_raport', [FormulirNilaiRaport::class, 'index'])->name('formulir_nilai_raport');
+    Route::post('/formulir_nilai_raport/save', [FormulirNilaiRaport::class, 'save_nilai_raport'])->name('save_nilai_raport');
+    Route::put('/formulir_nilai_raport/edit/{nilai_raport}', [FormulirNilaiRaport::class, 'edit_nilai_raport'])->name('edit_nilai_raport');
 
-// });
+});
 
-// Route::middleware(['auth', 'preventBackHistory', 'checkProfile', 'checkDataSiswa', 'checkDataOrangTua', 'checkDataPeriodik', 'checkNilaiRaport'])->group(function () {
-//     // Upload Berkas
-//     Route::get('/upload_berkas', function () {
-//         return view('uploads.index', ['user' => Auth::user()]);
-//     })->name('upload_berkas');
+// Middleware formulir upload berkas
+Route::middleware(['auth', 'preventBackHistory', 'checkProfile', 'checkDataSiswa', 'checkDataOrangTua', 'checkDataPeriodik', 'checkNilaiRaport'])->group(function () {
+    // Upload Berkas
+    Route::get('/upload_berkas', function () {
+        return view('uploads.index', ['user' => Auth::user()]);
+    })->name('upload_berkas');
 
-//     // ID Card
-//     Route::get('/id_card', function () {
-//         return view('id-card.index');
-//     })->name('id_card');
+    // ID Card
+    Route::get('/id_card', function () {
+        return view('id-card.index');
+    })->name('id_card');
 
-//     // cetak ID Card
-//     Route::get('/idcard/pdf/{id}', function ($id) {
-//         $item = IdCard::findOrFail($id);
-//         $width = 8.56 * 28.3465; // 242.5pt
-//         $height = 5.4 * 28.3465; // 153.1pt
-//         $pdf = Pdf::loadView('id-card.template', compact('item'))
-//             ->setPaper([0, 0, $height, $width], 'potrait');
+    // cetak ID Card
+    Route::get('/idcard/pdf/{id}', function ($id) {
+        $item = IdCard::findOrFail($id);
+        $width = 8.56 * 28.3465; // 242.5pt
+        $height = 5.4 * 28.3465; // 153.1pt
+        $pdf = Pdf::loadView('id-card.template', compact('item'))
+            ->setPaper([0, 0, $height, $width], 'potrait');
 
-//         return $pdf->stream('idcard_' . $item->nama . '.pdf');
-//     });
-// });
-
+        return $pdf->stream('idcard_' . $item->nama . '.pdf');
+    });
+});
 
 // // Middleware untuk formulir registrasi
-// Route::middleware(['auth', 'preventBackHistory', 'checkProfile', 'checkDataSiswa', 'checkDataOrangTua', 'checkDataPeriodik','checkUploadBerkas'])->group(function () {
-//     // Formulir Registrasi
-//     Route::get('/formulir_registrasi', [FormulirRegistrasi::class, 'index'])->name('formulir_registrasi');
-//     Route::post('/formulir_registrasi/save', [FormulirRegistrasi::class, 'save_registrasi'])->name('save_formulir_registrasi');
-// });
+Route::middleware(['auth', 'preventBackHistory', 'checkProfile', 'checkDataSiswa', 'checkDataOrangTua', 'checkDataPeriodik','checkUploadBerkas'])->group(function () {
+    // Formulir Registrasi
+    Route::get('/formulir_registrasi', [FormulirRegistrasi::class, 'index'])->name('formulir_registrasi');
+    Route::post('/formulir_registrasi/save', [FormulirRegistrasi::class, 'save_registrasi'])->name('save_formulir_registrasi');
+});
 
 Route::get('/cekpendaftar/{registrasi}', [CekPendaftarController::class, 'show'])->name('cek_pendaftar')->middleware('signed');
 
